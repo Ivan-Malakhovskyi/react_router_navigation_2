9a4ad1bf8af8413ba287c64fccf19e73850363f8
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2026-08-27 16:09:02 +0300

    update axios default values

--- src/services/articlesServices.js
+++ src/services/articlesServices.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://newsapi.org/v2/";
-axios.defaults.params = {
-  apiKey: "8cb01996c0d34dbebd5fe5c1bf4080cf",
-  pageSize: 6,
-  searchIn: "title",
-};
+const axiosInstance = axios.create({
+  baseURL: "https://newsapi.org/v2/",
+  params: {
+    apiKey: "8cb01996c0d34dbebd5fe5c1bf4080cf",
+    pageSize: 6,
+    searchIn: "title",
+  },
+});
 
 export const getArticles = async (query, page = 1) => {
-  const { data } = await axios.get("everything", {
+  const { data } = await axiosInstance.get("everything", {
     params: { q: query || "javascript", page },
   });
   return data;
 };
 
 export const getSingeArticleService = async (query) => {
-  const { data } = await axios.get("everything", {
+  const { data } = await axiosInstance.get("everything", {
     params: {
       q: query,
       searchIn: "title",
